114403082d26b225db3cfc9f7ad2747b16703f3b
Utee Ekong <utibeabasiekong2@gmail.com>
2025-02-24 01:26:16 +0100

    update: see serial_port to show test logs on console and not qemu

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,31 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+/// This function is called on panic.
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -55,7 +66,7 @@ pub extern "C" fn _start() -> ! {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion...");
-    assert_eq!(1, 1);
-    println!("[ok]");
+    serial_print!("trivial assertion...");
+    assert_eq!(1, 0);
+    serial_println!("[ok]");
 }
