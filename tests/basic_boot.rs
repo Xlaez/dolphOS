7b73497babef49150a88f85c3f703e271c83b2c0
Utee Ekong <utibeabasiekong2@gmail.com>
2025-03-04 19:18:02 +0100

    update: add integration test support

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,16 +1,33 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
+#![test_runner(dolph_os::test_runner)]
+
+use dolph_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
+/*
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(dolph_os::test_runner)]
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    test_main();
+// #[no_mangle]
+// pub extern "C" fn _start() -> ! {
+    // test_main();
 
-    loop {}
+    // loop {}
+// }
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
 
 fn test_runner(tests: &[&dyn Fn()]) {
@@ -21,3 +38,4 @@ fn test_runner(tests: &[&dyn Fn()]) {
 fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
+*/
